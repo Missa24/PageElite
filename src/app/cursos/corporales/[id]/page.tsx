c7ff4a59a3e08f0aad8d4cc75172fd50ef0a5ebf
Missael <missaelapaza@gmail.com>
2026-08-23 23:22:39 -0400

    modify params in page id

--- src/app/cursos/corporales/[id]/page.tsx
+++ src/app/cursos/corporales/[id]/page.tsx
@@ -1,16 +1,26 @@
+import { notFound } from "next/navigation";
 import { Background } from "@/components/background";
 import { CourseDetail } from "@/components/cursos/course-detail";
 import { cursosCorporales } from "@/data/curso-detalle/corporales";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const curso = cursosCorporales.find((curso) => curso.id === id);
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const curso = cursosCorporales.find(
+    (curso) => curso.id === id
+  );
+
+  if (!curso) {
+    notFound();
+  }
 
   return (
     <Background>
-      <CourseDetail
-        curso={curso!}
-      />
+      <CourseDetail curso={curso} />
     </Background>
   );
 }
--- src/app/cursos/maquillaje/[id]/page.tsx
+++ src/app/cursos/maquillaje/[id]/page.tsx
@@ -1,16 +1,26 @@
+import { notFound } from "next/navigation";
 import { Background } from "@/components/background";
 import { CourseDetail } from "@/components/cursos/course-detail";
 import { cursosMaquillaje } from "@/data/curso-detalle/maquillaje";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const curso = cursosMaquillaje.find((curso) => curso.id === id);
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const curso = cursosMaquillaje.find(
+    (curso) => curso.id === id
+  );
+
+  if (!curso) {
+    notFound();
+  }
 
   return (
     <Background>
-      <CourseDetail
-        curso={curso!}
-      />
+      <CourseDetail curso={curso} />
     </Background>
   );
 }
